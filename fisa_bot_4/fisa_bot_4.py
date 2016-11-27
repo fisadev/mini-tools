957cc67c5c89cf24df102501ca9537e1c3c89e58
Juan Pedro Fisanotti <fisadev@gmail.com>
2016-11-27 16:44:31 -0300

    Nicer code

--- fisa_bot_4/fisa_bot_4.py
+++ fisa_bot_4/fisa_bot_4.py
@@ -2,10 +2,8 @@ import requests
 
 
 for i in range(1000):
-    print(i)
     user_id = 'f' * 8 + str(i).rjust(8, '0')
     response = requests.post('http://pyconar-talks.fiqus.com/api/scores',
                              json={'score': {'score': 5, 'talk_id': 4, 'user_id': user_id}})
 
-    if response.status_code != 201:
-        print('failed:', user_id, response.status_code, response.content)
+    print(user_id, response.status_code, response.content)
